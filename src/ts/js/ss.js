fcca998eacfc6a41e2121d95fd6ac657a247e47c
carafelix <heroprotagonist32@gmail.com>
2023-07-10 16:15:34 -0400

    add html2canvas via cdn and timout plus transition

---
+++ src/ts/js/ss.js
@@ -0,0 +1,28 @@
+const   ssBtn = document.querySelector("#screenshot"); // El botón que desencadena
+const   canvas = document.querySelector("#paint"); // A qué le tomamos la foto
+
+// Agregar el listener al botón
+ssBtn.addEventListener("click", () => {
+
+    canvas.classList.toggle('left-shadow');
+    setTimeout(() => {
+        canvas.classList.toggle('left-shadow');
+      }, 3000);
+
+
+  html2canvas(canvas) // Llamar a html2canvas y pasarle el elemento
+    .then(canvas => {
+      // Cuando se resuelva la promesa traerá el canvas
+      // Crear un elemento <a>
+      let link = document.createElement('a');
+      link.download = "Your-art";
+      // Convertir la imagen a Base64
+      link.href = canvas.toDataURL();
+      
+      // Hacer click en él
+      link.click();
+    })
+});
+
+
+
